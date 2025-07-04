8a87525e667b7b97fc4a6094566cdceb3e29fe96
AmirAzad033 <AmirAzad033@gmail.com>
2025-07-04 09:00:00 +0200

    Ui improvement

---
+++ cell.h
@@ -0,0 +1,30 @@
+#ifndef CELL_H
+#define CELL_H
+
+#include <vector>
+#include "Agent.h"
+
+enum class TerrainType {
+    Free,
+    Water,
+    Rock
+};
+
+class Cell {
+public:
+    int row;
+    int col;
+    TerrainType terrain;
+    int owner;
+
+    std::vector<Cell*> neighbors;
+    bool visited = false;
+    Cell* parent = nullptr;
+    int distance = -1;
+    Agent* occupiedAgent = nullptr;
+
+    Cell();
+    Cell(int r, int c, TerrainType t, int o = 0);
+};
+
+#endif
